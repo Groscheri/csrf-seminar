0616ca36e7b71d394c7e6b285604cf173e028fc3
Quentin Lemaire <quentyn.lemaire@gmail.com>
2015-11-27 00:52:01 +0100

    Interest class new functions and add interests
    - Add interest form in webview
    - Function "all" to retrieve all interests
    - Function "get_by_user" to retrieve all interests of a user
    - Function "bind_user" to bind an interest with a user
    - Function "add_and_bind_user" to add an interest and bind it with a user

--- webapp/core/interest.class.php
+++ webapp/core/interest.class.php
@@ -2,8 +2,30 @@
 // interest class
 
 class interest {
+    public static function all() {
+        $sql = 'SELECT `id`, `name`, `description` FROM `interests`';
+        $params = array();
+        try {
+            return DB::Prepare($sql, $params, DB::FETCH_TYPE_ALL);
+        }
+        catch (Exception $e) {
+            return -1;
+        }
+    }
+
+    public static function get_by_user($_user_id) {
+        $sql = 'SELECT i.`id`, i.`name`, i.`description` FROM `interests` i, `users_interests` ui WHERE ui.`user_id` = :user_id AND ui.`interest_id` = i.`id`;';
+        $params = array('user_id' => $_user_id);
+        try {
+            return DB::Prepare($sql, $params, DB::FETCH_TYPE_ALL);
+        }
+        catch (Exception $e) {
+            return -1;
+        }
+    }
+
     public static function get($_id) {
-        $sql = 'SELECT `name`, `description` FROM `interests` WHERE `id` = :id';
+        $sql = 'SELECT `id`, `name`, `description` FROM `interests` WHERE `id` = :id';
         $params = array('id' => $_id);
         try {
             return DB::Prepare($sql, $params, DB::FETCH_TYPE_ALL);
@@ -17,6 +39,27 @@ class interest {
         $sql = 'INSERT INTO `interests` (`name`, `description`) VALUES (:name, :description)';
         $params = array('name'        => $_name,
                         'description' => $_description);
+        try {
+            DB::Prepare($sql, $params);
+            return DB::last_insert_id();
+        }
+        catch (Exception $e) {
+            return false;
+        }
+    }
+
+    public static function add_and_bind_user($_name, $_description, $_user_id) {
+        $id = self::add($_name, $_description);
+        if ($id === false) {
+            return false;
+        }
+        return self::bind_user($id, $_user_id);
+    }
+
+    public static function bind_user($_id, $_user_id) {
+        $sql = 'INSERT INTO `users_interests` (`interest_id`, `user_id`) VALUES (:id, :user_id);';
+        $params = array('id'      => $_id,
+                        'user_id' => $_user_id);
         try {
             return DB::Prepare($sql, $params);
         }
--- webapp/view/interests.php
+++ webapp/view/interests.php
@@ -7,6 +7,28 @@ if (!$logged) {
     redirect("?p=connect&error=2");
     die();
 }
+
+if (isset($_GET['action'])) {
+    // handle actions
+
+    $action = $_GET['action'];
+    if ($action == 'add') {
+        if (!empty($_POST['name']) && isset($_POST['description'])) {
+            $result = interest::add_and_bind_user($_POST['name'], $_POST['description'], $_SESSION['user']['id']);
+            if ($result === false) {
+                echo '<p>Impossible to add a new interest!</p>';
+            }
+            echo '<p>Interest has been created!</p>';
+        }
+    }
+    elseif ($action == 'remove') {
+        // TODO
+    }
+    else {
+        // unknown action
+    }
+
+}
 ?>
 
 <h2>Interests list</h2>
@@ -14,23 +36,31 @@ if (!$logged) {
 <?php
 
 // list interests
-$interests = interest::get($_SESSION['user']['id']);
+$interests = interest::get_by_user($_SESSION['user']['id']);
 
 if (!is_array($interests) || count($interests) == 0) {
 ?>
 <p>No interest at the moment!</p>
 <?php
 }
-
-echo '<ul>';
-foreach ($interests as $interest) {
-    $name = $interest['name'];
-    $description = $interest['description']; 
-    echo '<li>' . $name . ': ' . $description . '</li>';    
+else {
+    echo '<ul>';
+    foreach ($interests as $interest) {
+        $id = $interest['id'];
+        $name = $interest['name'];
+        $description = $interest['description']; 
+        echo '<li>' . $name . ': ' . $description . ' <a href=\'?p=interests&action=remove&id=' . $id . '\' title=\'Remove this interest\'>Remove</a></li>';    
+    }
+    echo '</ul>';
 }
-echo '</ul>';
-
-// TODO add interest
-// with multiselect
-
 ?>
+
+<h3>Create new interest</h3>
+
+<form method="POST" action="?p=interests&action=add">
+<label for="name">Name: </label><input type="text" name="name" id="name" /><br />
+<label for="description">Description: </label><br />
+<textarea name="description" placeholder="Description optional"></textarea>
+<br />
+<input type="submit" value="Create interest" />
+</form>
